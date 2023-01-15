6a3a9607e9381e660caa0cdd725d85b2f2cd37db
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-01-15 21:29:29 +0000

    add wrapper for Tweet

---
+++ src/twit2imgs/models.py
@@ -0,0 +1,29 @@
+import tweepy
+from PIL import Image
+import requests
+from io import BytesIO
+
+class Tweet(ABC):
+    def __init__(self, ttweet, img_urls):
+        self.id: str = ttweet.id
+        self.text: str = ttweet.text
+        self.image_url: str = img_urls[ttweet.attachments['media_keys'][0]]
+        self.ttweet: tweepy.tweet.Tweet = ttweet
+        
+        r = requests.get(self.image_url)
+        r.raise_for_status()
+        
+        self.image = Image.open(BytesIO(r.content))
+        
+    def write_record(self, fpath):
+        
+        record = dict(
+            id=self.id,
+            text = self.text,
+            url = self.image_url,
+        )
+        
+        json.dump(record, open(fpath, 'w'))
+        
+        return 1
+        
