3ef645d8a2b243701e6ad1da07683420380a8134
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-02-04 11:39:46 +0000

    small tweaks to image prep

--- src/twit2imgs/image_utils.py
+++ src/twit2imgs/image_utils.py
@@ -1,6 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
 import re
+import os
 import string, random
+
+def format_s2_url(url: str) -> str:
+    base, ext = os.path.splitext(url)
+    return base+"?format=jpg&name=4096x4096"
+
+def null_url_parser(url: str) -> str:
+    return url
 
 def beautiful_s2_to_16_9_labelled(
     im: Image, 
@@ -28,12 +36,12 @@ def beautiful_s2_to_16_9_labelled(
     left_align = im.width - int(max(len(top_line), len(bottom_line))*pix_per_char) - 10
 
     draw = ImageDraw.Draw(im, 'RGBA')
-    draw.rounded_rectangle((left_align-25, 1155, 2300, 1300), fill=(255,255,255,130), outline=None,  width=3, radius=20)
+    draw.rounded_rectangle((left_align-25, 1155, 2300, 1300), fill=(255,255,255,176), outline=None,  width=3, radius=20)
 
     top_row_anchor = (left_align,1170)
     bottom_row_anchor = (left_align,1200)
 
-    draw.text(top_row_anchor,top_line,(255,255,255,128), font=font)
-    draw.text(bottom_row_anchor,bottom_line,(255,255,255,128), font=font)
+    draw.text(top_row_anchor,top_line,(120,120,120,196), font=font)
+    draw.text(bottom_row_anchor,bottom_line,(120,120,120,196), font=font)
     
     return im
