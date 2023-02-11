7546a03df44afd6b7f719208270f76ae1d9cb93c
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-02-11 10:37:46 +0000

    tie slackbot into cli

--- src/twit2imgs/cli.py
+++ src/twit2imgs/cli.py
@@ -10,6 +10,7 @@ from loguru import logger
 from dotmap import DotMap
 
 from twit2imgs import utils
+from twit2imgs.slackbot import SlackBot
 
 logger.remove()
 logger.add(sys.stdout, colorize=False, format="{time:YYYYMMDDHHmmss}|{level}|{message}")
@@ -32,6 +33,15 @@ def DAG(conf_path):
     # set up tmp dir
     if not os.path.exists('tmp'):
         os.makedirs("tmp/")
+        
+    slack_token = os.environ.get('SLACKBOT_TOKEN')
+    slack_channel = os.environ.get('SLACKBOT_CHANNEL')
+    if slack_token is not None and slack_channel is not None:
+        slackbot = SlackBot(token=slack_token, channel=slack_channel, name='twit2imgs')
+    else:
+        slackbot=None
+    
+    msg = {}
     
     # parse config
     logger.info('parsing config')
@@ -43,24 +53,32 @@ def DAG(conf_path):
     scraper = utils._indirect_cls(cfg.scraper.cls)(**cfg.scraper.params)
     tweets = scraper.scrape()
     logger.info(f'Got {len(tweets)} tweets')
+    msg['scraped_tweets']=len(tweets)
     
     # store tweet records and images to cloud
     logger.info('storing tweets')
     storer = utils._indirect_cls(cfg.storer.cls)(**cfg.storer.params)
     if storer is not None:
         storer.store(tweets)
+        msg['storer'] = cfg.storer.cls.split('.')[-1]
         
     # for each target: preprocess, post_tweets, postprocess
+    msg['targets'] = []
     for target_key, target_params in cfg.targets.items():
         logger.info(f'updating target: {target_key}')
         target = utils._indirect_cls(target_params.cls)(**target_params.params)
         target.preprocess()
         target.post_tweets(tweets)
         target.postprocess()
+        msg['targets'].append(target_key)
         
     # cleanup
     shutil.rmtree("tmp/")
     os.makedirs("tmp/")
+    
+    if slackbot is not None:
+        slackbot.post(msg)
+    
         
     return 200
 
